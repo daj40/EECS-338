46d6b2cf8cc7b1825452651660371c7cb42ad169
Dave Jannotta <daj40@case.edu>
2014-03-23 12:28:20 -0400

    Added AS4, initial commit for assignment 4

---
+++ AS4/main.c
@@ -0,0 +1,21 @@
+
+//==============================================//
+//  Name		: main.c 						//
+//	Author		: David Jannotta				//
+//	Caseid		: daj40							//
+//	Description	: EECS 338 Homework 4			//
+//==============================================//
+
+
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+
+
+
